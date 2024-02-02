cec2b3db3f82296da0904ec027b3f10da98e1cb5
Lucas Costa Souza <lucas_costasouza@yahoo.com.br>
2024-02-02 17:27:44 -0300

    Aula 1 - Atividade 09 - Completo

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ load_dotenv()
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 resposta = cliente.chat.completions.create(
-        messages = [
-                {
-                        "role" : "system",
-                        "content" :"Listar apenas os nomes dos produtos, sem considerar descrição."
-                },
-                {
-                        "role" : "user",
-                        "content" :"Liste 3 produtos sustentáveis"
-                }
-        ],
-        model = "gpt-4"
+    messages = [
+        {
+            "role" : "system",
+            "content" :"Listar apenas os nomes dos produtos, sem considerar descrição."
+        },
+        {
+            "role" : "user",
+            "content" :"Liste 3 produtos sustentáveis"
+        }
+    ],
+    model = "gpt-4"
 )
 
 print(resposta.choices[0].message.content)
