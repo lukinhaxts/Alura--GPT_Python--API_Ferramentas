8b1788ec049b7f24ce2bec0605a97b85c88dac2a
Lucas Costa Souza <lucas_costasouza@yahoo.com.br>
2024-02-05 10:21:10 -0300

    Aula 02 - Atividade 03

--- categorizador.py
+++ categorizador.py
@@ -4,27 +4,44 @@ import os
 
 load_dotenv()
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+modelo = "gpt-4"
+
+prompt_sistema = f"""
+    Você é um categorizador de produtos.
+    Você deve assumir as categorias presentes na lista abaixo.
+
+    # Lista de Categorias Válidas
+        - Moda Sustentável
+        - Produtos para o Lar
+        - Beleza Natural
+        - Eletrônicos Verdes
+        - Higiene pessoal
+
+    # Formato da Saída
+    Produto: Nome do Produto
+    Categoria: apresente a categoria do produto
+
+    # Exemplo de Saída
+    Produto: Escova elétrica com recarga solar
+    Categoria: Eletrônicos Verdes
+"""
+
+prompt_usuário = input("Apresente o nome de um produto: ")
 
 resposta = cliente.chat.completions.create(
     messages = [
         {
             "role" : "system",
-            "content" : """
-            Classifique o produto abaixo em uma das categorias: Higiene Pessoal, Moda ou Casa e de uma descrição da categoria.
-            """
+            "content" : prompt_sistema
         },
         {
             "role" : "user",
-            "content" : """
-            Escova de dentes de bambu
-            """
+            "content" : prompt_usuário
         }
     ],
-    model = "gpt-4",
+    model = modelo,
     temperature = 0,
-    max_tokens = 200,
-    n = 3
+    max_tokens = 200
 )
 
-for contador in range(0,3):
-    print(resposta.choices[contador].message.content)
+print(resposta.choices[0].message.content)
